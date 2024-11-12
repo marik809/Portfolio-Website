320aacdfa700099938d9387b31432f702fe39883
marik809 <marik809.antonio@gmail.com>
2024-11-12 17:44:45 -0500

    Add files via upload

---
+++ website.js
@@ -0,0 +1,12 @@
+/*
+var password = "12345";
+
+function passcheck () {
+    if (document.getElementById('pass1').value != password) {
+        alert('Contact Me for the password');
+        return false;
+    }
+    if (document.getElementById('pass1').value != password) {
+        return true;
+    }    
+}
